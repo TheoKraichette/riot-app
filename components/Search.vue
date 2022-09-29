be6478ef09ae6ea4cc5270d41081cb54a5d7ce38
xSweak <kraichettedev@gmail.com>
2022-09-29 14:05:48 +0200

    requete plus simple

--- components/Search.vue
+++ components/Search.vue
@@ -1,13 +1,18 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet" />
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
       <input type="text" v-model="myInput" />
       <input type="submit" v-on:click="getData()" />
       <div>{{ username }} {{ level }}</div>
       <div>Wins : {{ wins }}</div>
-      <div>Looses : {{ looses }}</div>
+      <div>losses : {{ losses }}</div>
       <div>Winrate : {{ winrate }}</div>
     </div>
   </div>
@@ -20,14 +25,13 @@ export default {
   name: "Search",
   data() {
     return {
-      userPuuid: "",
+      userId: "",
       username: "Who?",
       level: "which level ?",
-      listMatch: [],
       wins: 0,
-      looses: 0,
+      losses: 0,
       winrate: 0,
-      apiKey: "RGAPI-78ab5886-ef8e-4ab6-9166-e242d6f2d593" ,
+      apiKey: "RGAPI-78ab5886-ef8e-4ab6-9166-e242d6f2d593",
     };
   },
   methods: {
@@ -36,15 +40,16 @@ export default {
         axios
           .get(
             "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" +
-            this.myInput +
-            "?api_key=" + this.apiKey
+              this.myInput +
+              "?api_key=" +
+              this.apiKey
           )
           .then((res) => {
             if (res.data) {
-              (this.userPuuid = res.data.puuid),
+              (this.userId = res.data.id),
                 (this.username = res.data.name),
                 (this.level = "is level " + res.data.summonerLevel);
-              this.getListMatch(this.userPuuid);
+              this.getWinLoose(this.userId);
             }
           })
           .catch((err) => {
@@ -54,42 +59,24 @@ export default {
           });
       }
     },
-    getListMatch(userPuuid) {
+    getWinLoose(userId) {
       axios
         .get(
-          "https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/" +
-          userPuuid +
-          "/ids?start=0&count=19&api_key=" + this.apiKey
+          "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/" +
+            userId +
+            "?api_key=" +
+            this.apiKey
         )
         .then((res) => {
-          this.listMatch = res.data;
-          this.getWinLoose(this.listMatch);
+          console.log(res);
+          this.wins = res.data[0].wins;
+          this.losses = res.data[0].losses;
+          this.getWinrate();
         });
-    },
-    getWinLoose(listMatch) {
-      listMatch.forEach((match) => {
-        axios
-          .get(
-            "https://europe.api.riotgames.com/lol/match/v5/matches/" +
-            match +
-            "?api_key=" + this.apiKey
-          )
-          .then((res, win) => {
-            res.data.info.participants.forEach((user) => {
-              if (user.puuid == this.userPuuid) {
-                win = user.win;
-                if (win) {
-                  this.wins += 1;
-                } else this.looses += 1;
-              }
-            });
-            this.getWinrate();
-          });
-      });
     },
     getWinrate() {
       return (this.winrate =
-        ((this.wins / (this.wins + this.looses)) * 100).toFixed(2) + "%");
+        ((this.wins / (this.wins + this.losses)) * 100).toFixed(2) + "%");
     },
   },
 };
